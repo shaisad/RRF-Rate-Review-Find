9d84f007a0e78780442de397781de41ae73f5fc2
Shaira Sadia Karim <shairasadia@iut-dhaka.edu>
2022-11-14 13:59:40 +0600

    add result

--- SPL1-(R.R.F)/HomePageLoggedInUser.php
+++ SPL1-(R.R.F)/HomePageLoggedInUser.php
@@ -248,6 +248,7 @@ img {
            <div class="search-box">
               <input type="text" placeholder="Type to search for restaurants and foods..."> </div>
            <div class="search-btn">
+           <div class = "result"></div> 
               <i class="fa fa-search"></i> </div>
     </div>
 
