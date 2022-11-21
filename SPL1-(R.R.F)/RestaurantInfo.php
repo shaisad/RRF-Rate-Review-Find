75e8375ace832a39fd78d02c44cca5347806e615
Shaira Sadia Karim <shairasadia@iut-dhaka.edu>
2022-11-21 16:55:42 +0600

    modify

---
+++ SPL1-(R.R.F)/RestaurantInfo.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <title>Restaurant's Info Page</title>
+</head>
+
+<style>
+
+body{
+    background-color: rgb(240, 221, 136);
+    overflow-x: hidden;
+}
+
+.rrflogo {
+    width: 200px;
+    position: relative;
+    bottom: 100px;
+    /* padding-bottom: 20px;
+    margin-bottom: 40px; */
+}
+
+ .foodcard {
+    margin-bottom: 40px;
+    max-height: 218x;
+    }  
+
+.card{
+    background-color:rgb(240, 221, 136);;
+
+}
+
+.foodimg{
+    width: 325px;
+    height: 325px;
+    object-fit: cover;
+}
+
+.card-body{
+    position: relative;
+    right: 125px;
+    background-color: aliceblue;
+    width: 1023px;
+    overflow-y: scroll;
+}
+
+.rate {
+    float: left;
+    height: 46px;
+    padding: 0 10px;
+    position: relative;
+    bottom: 50px;
+    left: 80px;
+}
+.rate:not(:checked) > input {
+    position:absolute;
+    top:-9999px;
+}
+.rate:not(:checked) > label {
+    float:right;
+    width:1em;
+    overflow:hidden;
+    white-space:nowrap;
+    cursor:pointer;
+    font-size:30px;
+    color:#ccc;
+}
+.rate:not(:checked) > label:before {
+    content: '★ ';
+}
+.rate > input:checked ~ label {
+    color: #ffc700;    
+}
+.rate:not(:checked) > label:hover,
+.rate:not(:checked) > label:hover ~ label {
+    color: #deb217;  
+}
+.rate > input:checked + label:hover,
+.rate > input:checked + label:hover ~ label,
+.rate > input:checked ~ label:hover,
+.rate > input:checked ~ label:hover ~ label,
+.rate > label:hover ~ input:checked ~ label {
+    color: #c59b08;
+}
+
+.cardbtn a {
+  border: none;
+  outline: 0;
+  display: inline-block;
+  padding: 9px;
+  margin-right: 10px;
+  color: white;
+  background-color: rgb(80, 31, 19);
+  text-align: center;
+  cursor: pointer;
+  width: 200px;
+  font-size: 18px;
+  position: relative;
+  right: 170px;
+  top: 5px;
+}
+
+.cardbtn a:hover
+{
+  opacity: 0.7;
+}
+
+.restname{
+    color: rgb(80, 31, 19);
+    text-align: left;
+    position: relative;
+    left: 470px;
+}
+
+.cardi{
+    position: relative;
+    bottom: 50px;
+
+}
+
+.handrice{
+    width: 60px;
+    position: relative;
+    left: 400px;
+    top: 65px;
+}
+
+</style>
+
+<body>
+
+    <div class="logo">
+
+        <p>
+            <img class="handrice" src="handrice.png" alt="logo">
+            <h1 class="restname">Restaurant's Name</h1>
+            <img class="rrflogo" src="cover.png" alt="logo">
+        </p>
+
+    </div>
+
+<div class="cardi">
+
+  <div class="foodcard">
+    <div class="card mb-3" style="max-width: 100%;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img class="foodimg" src="Photos 18_10_2022 8_31_57 PM.png" class="img-fluid rounded-start" alt="..."/>
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Food's Name</h5>
+              <p>Food Description</p>
+              <p>Category</p>
+              <p>Price</p>
+              <a class="card-link stretched-link" href="FoodProfile.html"></a>
+              <p>
+                <p>Totalrates  Totalreviews</p>
+              </p>
+
+            <p>
+               Rate Here:
+                <div class="rate">
+                    <input type="radio" id="star5" name="rate" value="5" />
+                    <label for="star5" title="text">5 stars</label>
+                    <input type="radio" id="star4" name="rate" value="4" />
+                    <label for="star4" title="text">4 stars</label>
+                    <input type="radio" id="star3" name="rate" value="3" />
+                    <label for="star3" title="text">3 stars</label>
+                    <input type="radio" id="star2" name="rate" value="2" />
+                    <label for="star2" title="text">2 stars</label>
+                    <input type="radio" id="star1" name="rate" value="1" />
+                    <label for="star1" title="text">1 star</label>
+                </div>
+                <p>
+                    <div class="cardbtn">
+                    <a href="#" target="_self">Write a review</a>
+                    <a href="FoodProfile.html" target="_self">See Details</a>
+                    </div>
+                </p>
+             
+            </div>
+          </div>
+        </div>
+    </div>
+  </div>
+
+ 
+    <div class="card mb-3" style="max-width: 100%;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img class="foodimg" src="Photos 18_10_2022 8_32_21 PM.png" class="img-fluid rounded-start" alt="..."/>
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Food's Name</h5>
+              <p>Food Description</p>
+              <p>Category</p>
+              <p>Price</p>
+              <p>
+                <p>Totalrates  Totalreviews</p>
+              </p>
+
+            <p>
+               Rate Here:
+                <div class="rate">
+                    <input type="radio" id="star5" name="rate" value="5" />
+                    <label for="star5" title="text">5 stars</label>
+                    <input type="radio" id="star4" name="rate" value="4" />
+                    <label for="star4" title="text">4 stars</label>
+                    <input type="radio" id="star3" name="rate" value="3" />
+                    <label for="star3" title="text">3 stars</label>
+                    <input type="radio" id="star2" name="rate" value="2" />
+                    <label for="star2" title="text">2 stars</label>
+                    <input type="radio" id="star1" name="rate" value="1" />
+                    <label for="star1" title="text">1 star</label>
+                </div>
+                <p>
+                    <div class="cardbtn">
+                        <a href="#" target="_self">Write a review</a>
+                        <a href="FoodProfile.html" target="_self">See Details</a>
+                        </div>
+                </p>
+             
+            </div>
+          </div>
+        </div>
+    </div>
+  </div>
+
+  </div>
+</div>
+    
+</body>
+
+</html>
