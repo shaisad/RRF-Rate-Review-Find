fb3bfc29fee94b56261a28c527f8a968b932f24c
Shaira Sadia Karim <shairasadia@iut-dhaka.edu>
2022-12-25 04:27:07 +0600

    restaurant menu backend done

---
+++ SPL1-(R.R.F)/RestaurantMenu.php
@@ -0,0 +1,465 @@
+<?php
+session_start();
+?> 
+<?php
+$irestaurantname = $_SESSION['restaurantname']; 
+//$foodname = $_SESSION['foodname'];
+?> 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+<!-- Bootstrap CSS -->
+
+<link href="/css/bootstrap.min.css" rel="stylesheet">
+
+    <title>Restaurant's Info Page</title>
+</head>
+
+<style>
+
+@import url("https://fonts.googleapis.com/css??family=Poppins:wght@400;500;600;700&display=swap");
+
+
+body{
+    background-color: rgb(240, 221, 136);
+    overflow-x: hidden;
+}
+
+ .search{
+    box-sizing: border-box;
+    font-family: 'Poppins', sans-serif;
+    position: relative;
+    left: 30px ;
+    
+}
+
+.search-box input[type="text"]{
+    height: 100%;
+    width: 100%;
+    border: none;
+    outline: none;
+    background: #fff;
+    font-size: 18px;
+    padding: 0 60px 0 20px;
+}
+
+.search-box {
+    height: 50px;
+    width: 600px;
+    color: #fff;
+    position: relative;
+    left: 100px;
+    bottom: 15px;
+}
+
+
+.result{
+        position: absolute;        
+        z-index: 999;
+        top: 100%;
+        left: 0;
+    }
+
+.result p{
+        margin: 0;
+        padding: 7px 10px;
+        border: 1px solid #CCCCCC;
+        border-top: none;
+        cursor: pointer;
+    }
+
+.result p:hover{
+        background: #f2f2f2;
+    }
+
+
+.search-btn {
+    position: relative;
+    left: 550px;
+    bottom: 35px;
+    height: 50px;
+    width: 70px;
+    color: rgb(80, 31, 19) ;
+
+}
+
+.search-btn:hover {
+    color: rgb(100, 91, 148);
+}
+
+.btnn {
+  border: none;
+  outline: none;
+  padding: 8px 8px;
+  cursor: pointer;
+  color: rgb(80, 31, 19);
+  position: relative;
+  left: 125px;
+  top: 10px;
+  border-radius: 10px;
+}
+
+
+.btnn:hover {
+  background-color: rgb(155, 113, 58) ;
+  color: white;
+}
+
+
+.btnn.show {
+  background-color: #666;
+  color: white;
+}
+
+.cbtnn1 {
+  border: none;
+  outline: none;
+  padding: 8px 8px;
+  cursor: pointer;
+  color: white;
+  position: relative;
+  
+  top: 10px;
+  border-radius: 5px;
+  background-color: rgb(80, 31, 19) ;
+}
+
+
+.cbtnn1:hover {
+  opacity: 0.7; ;
+}
+
+.cbtnn2 {
+  border: none;
+  outline: none;
+  padding: 8px 8px;
+  cursor: pointer;
+  color: white;
+  position: relative;
+  left: 10px;
+  top: 10px;
+  border-radius: 5px;
+  background-color: rgb(80, 31, 19) ;
+}
+
+
+.cbtnn2:hover {
+  opacity: 0.7; ;
+}
+
+
+
+
+img {
+    width: 180px;
+    top: 300px;
+    left: 100px;
+}
+
+.sidebar {
+  height: 100%;
+  width: 180px;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  overflow-x: hidden;
+  background-color: rgb(241, 191, 114);
+  
+}
+
+.sidebar a {
+  padding: 6px 8px 6px 16px;
+  text-decoration: none;
+  font-size: 20px;
+  color: rgb(80, 31, 19);
+  display: block;
+  position: relative;
+  top: 10px;
+  left: 5px;
+}
+
+.sidebar a:hover {
+  color: gray;
+}
+
+.rate {
+    float: left;
+    height: 46px;
+    padding: 0 10px;
+    position: relative;
+    left: 25px;
+}
+.rate:not(:checked) > input {
+    position:absolute;
+    top:-9999px;
+}
+.rate:not(:checked) > label {
+    float:right;
+    width:1em;
+    overflow:hidden;
+    white-space:nowrap;
+    cursor:pointer;
+    font-size:30px;
+    color:#ccc;
+}
+.rate:not(:checked) > label:before {
+    content: '★ ';
+}
+.rate > input:checked ~ label {
+    color: #ffc700;    
+}
+.rate:not(:checked) > label:hover,
+.rate:not(:checked) > label:hover ~ label {
+    color: #deb217;  
+}
+.rate > input:checked + label:hover,
+.rate > input:checked + label:hover ~ label,
+.rate > input:checked ~ label:hover,
+.rate > input:checked ~ label:hover ~ label,
+.rate > label:hover ~ input:checked ~ label {
+    color: #c59b08;
+}
+
+.cardbtn a {
+  /* display: block;
+  border
+  color: white;
+  background-color: rgb(80, 31, 19);
+  text-align: center;
+  cursor: pointer;
+  width: 100%;
+  
+  padding: 16px 16px;
+  text-decoration: none;
+  font-size: 22px; */
+
+  border: none;
+  outline: none;
+  padding: 8px 8px;
+  cursor: pointer;
+  color: rgb(80, 31, 19);
+  /* position: relative;
+  left: 125px;
+  top: 10px;
+  border-radius: 10px; */
+
+
+}
+
+.cardbtn a:hover
+{
+  opacity: 0.7;
+}
+
+.cardfix {
+  position: relative;
+  left: 200px;
+  width: 80%;
+  cursor : pointer;
+}
+
+.card:hover {
+  box-shadow: 0 8px 16px 0 grey;
+  transform: translate3D(0,-1px,0) scale(1.03);
+} 
+
+
+.heading{
+  position: relative;
+  left: 220px;
+  top : 70px;
+  font-size: 25px;
+  color: rgb(80, 31, 19);
+}
+
+/* body{
+    background-color: rgb(240, 221, 136);
+    overflow-x: hidden;
+}
+img {
+    width: 1200px;
+    top: 200px;
+    left: 200px;
+}
+.cardfix {
+  position: relative;
+  left: 200px;
+  width: 80%;
+}
+.card:hover {
+  box-shadow: 0 8px 16px 0 grey;
+  transform: translate3D(0,-1px,0) scale(1.03);
+} 
+.rrflogo {
+    width: 200px;
+    position: relative;
+    bottom: 100px;
+}
+ 
+.restname{
+    color: rgb(80, 31, 19);
+    text-align: left;
+    position: relative;
+    left: 470px;
+}
+.handrice{
+    width: 60px;
+    position: relative;
+    left: 400px;
+    top: 65px;
+}
+.cbtnn1 {
+  border: none;
+  outline: none;
+  padding: 8px 8px;
+  cursor: pointer;
+  color: rgb(80, 31, 19);
+  position: relative;
+  left: 125px;
+  top: 10px;
+  border-radius: 10px;
+}
+.cbtnn1:hover {
+  background-color: rgb(155, 113, 58) ;
+  color: white;
+}
+img {
+    width: 50px;
+    top: 300px;
+    left: 100px;
+}
+.cardbtn {
+  border: none;
+  outline: 0;
+  display: inline-block;
+  padding: 8px;
+  color: white;
+  background-color: rgb(80, 31, 19);
+  text-align: center;
+  cursor: pointer;
+  width: 100%;
+  font-size: 18px;
+}
+.cardbtn:hover
+{
+  opacity: 0.7;
+}
+.cardfix {
+  position: relative;
+  left: 100px;
+  width: 10%;
+  
+}
+ */
+
+
+</style>
+
+<body>
+
+    <script>
+      document.getElementById("restnameresult").innerHTML=localStorage.getItem("rnamevalue");
+      document.getElementById("restlocationresult").innerHTML=localStorage.getItem("rlocationvalue");
+      document.getElementById("restimageresult").innerHTML=localStorage.getItem("rimagevalue");
+    </script>
+
+
+    <div class="logo">
+
+        <p>
+            <img class="handrice" src="handrice.png" alt="logo">
+
+            <h1 class="restname"><?php echo $_SESSION['restaurantname'];?></h1>
+            <h4 class="restlocation"><span id="restlocationresult"></span></h1>
+
+
+                <span id=restimageresult></span>
+                 
+            <img class="rrflogo" src="cover.png" alt="logo">
+        </p>
+
+    </div>
+
+    
+   
+
+      
+
+<!-- card -->
+
+<div class="cardfix">
+   <div class="container py-5">
+    <div class="row mt-3">
+       <?php 
+      require 'dbConfig.php';
+
+      $query = "SELECT * FROM food_new where frestaurantname = '$_SESSION[restaurantname]'";
+      $query_run = mysqli_query($db, $query);
+      $check_user = mysqli_num_rows($query_run) > 0;
+      
+      if($check_user)
+      {
+        while($row = mysqli_fetch_assoc($query_run))
+        {
+          ?>
+          
+          <div class="col-md-3 mt-3">
+            <div class="card">
+            
+          <div class="card-body">
+            <h5 class="card-title" id="rname"><?php echo $row['foodname']; ?></h5>
+            <p class="card-text" id="rlocation"><?php echo $row['subject']; ?></p>
+            <p class="card-text" id="rlocation"><?php echo $row['price']; ?></p>
+         
+            <!-- image fetch -->
+              <?php 
+          // Include the database configuration file  
+           require_once 'dbConfig.php'; 
+
+           $queryy = "SELECT image from foodimage where foodname = '$row[foodname]' and irestaurantname = '$_SESSION[restaurantname]' ";
+           $queryy_run = mysqli_query($db, $queryy);
+           $check_userr = mysqli_num_rows($queryy_run) > 0;
+
+           if($check_userr){
+            while($row = mysqli_fetch_assoc($queryy_run)){
+              ?>
+                 <img src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($row['image']); ?>" class="card-img-top" id="rimage"/>
+                 
+              <?php
+            }
+           }
+           ?>
+
+          
+        <button class="cbtnn1" onclick="window.location.href='reviewsection.php';">Write a review</button>
+        
+        
+
+  
+          </div>
+          </div>
+          </div>
+
+          <?php
+        }
+      }
+
+      ?>
+    </div>
+    </div>
+    </div>
+   
+
+</body>
+
+</html>
