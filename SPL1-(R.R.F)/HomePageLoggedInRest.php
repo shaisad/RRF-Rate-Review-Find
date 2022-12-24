bb77f0703170913ef189f8137e5bfd1d10f7ebb6
Shaira Sadia Karim <shairasadia@iut-dhaka.edu>
2022-12-25 04:26:42 +0600

    menu modify

--- SPL1-(R.R.F)/HomePageLoggedInRest.php
+++ SPL1-(R.R.F)/HomePageLoggedInRest.php
@@ -268,7 +268,7 @@ img {
         <a href="RestaurantProfile.php"><i class="fa fa-fw fa-user"></i>Restaurant's Profile</a>
         <a href="enter-restemail.html"><i class="fa fa-fw fa-key"></i>Reset Password</a>
         <a href="restaurantlogout.php"><i class="fa fa-fw fa-sign-out"></i>Log Out</a>
-        <a href="RestaurantInfo.php"><i class="fas fa-soup"></i>Menu</a>
+        <a href="RestaurantMenu.php"><i class="fas fa-soup"></i>Menu</a>
       </div> 
 
   
