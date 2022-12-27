b3eb87918a1cb38187ca10b16f6f66149d0066d2
turna62 <lamiaalam@iut-dhaka.edu>
2022-12-28 04:03:30 +0600

    check

---
+++ SPL1-(R.R.F)/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    
+    
+            <!-- Brand List  -->
+            
+                            <h5>Filter 
+                                <button type="submit" class="btn btn-primary btn-sm float-end">Search</button>
+                            </h5>
+                     
+                        <div class="card-body">
+                            <h6>Brand List</h6>
+                            <hr>
+                            <?php
+                                $con = mysqli_connect("localhost","root","","rrf");
+
+                                $brand_query = "SELECT * FROM restaurant";
+                                $brand_query_run  = mysqli_query($con, $brand_query);
+
+                                if(mysqli_num_rows($brand_query_run) > 0)
+                                {
+                                    foreach($brand_query_run as $brandlist)
+                                    {
+                                        $checked = [];
+                                        if(isset($_GET['cuisine']))
+                                        {
+                                            $checked = $_GET['cuisine'];
+                                        }
+                                        ?>
+                                            <div>
+                                                <input type="checkbox" name="cuisine[]" value="<?= $brandlist['restaurant']; ?>" 
+                                                    <?php if(in_array($brandlist['restaurantid'], $checked)){ echo "checked"; } ?>
+                                                 />
+                                                <?= $brandlist['name']; ?>
+                                            </div>
+                                        <?php
+                                    }
+                                }
+                                else
+                                {
+                                    echo "No Brands Found";
+                                }
+                            ?>
+                        </div>
+                    
+
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
