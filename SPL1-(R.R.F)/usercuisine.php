31937fa62689de1d899481de618948f4b48dc764
turna62 <lamiaalam@iut-dhaka.edu>
2022-12-27 09:43:46 +0600

    user cuisine

--- SPL1-(R.R.F)/usercuisine.php
+++ SPL1-(R.R.F)/usercuisine.php
@@ -21,20 +21,16 @@ if (isset($_POST['submitcuisine'])) {
     $query = "update user set userfavcuisine = '$_POST[userfavcuisine]' where username = '$_SESSION[username]'";
     $result = mysqli_query($conn, $query);
     if ($result) {
-        if ($result->num_rows == 1) {
-            
-?>      
-          <script>alert("Your favourite cuisine has been submitted successfully!")</script>
-          <?php
-            exit();
+      ?>      
+      <script>alert("Your favourite cuisine has been submitted successfully!")</script>
+      <?php
+      exit();
+    }
 
-
-
-        } else { ?>
-        <script>alert("Error!")</script>
-              <?php
-            exit();
-        }
+    else{?>
+      <script>alert("Error! Your favourite cuisine could not be submitted! Please try again!")</script>
+            <?php
+      exit();
     }
 }
 else{
