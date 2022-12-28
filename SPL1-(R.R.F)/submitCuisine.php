81cd9f0ae616319d3d98f4ece49faeaaac0bee59
turna62 <lamiaalam@iut-dhaka.edu>
2022-12-28 16:00:31 +0600

    check cuisine fix

--- SPL1-(R.R.F)/submitCuisine.php
+++ SPL1-(R.R.F)/submitCuisine.php
@@ -366,7 +366,7 @@ img {
     
     if(isset($_GET['cuisine']))
     {
-    ?> <p class = heading> All restaurants of <?php echo '$_GET[cuisine]';?> cuisine)</p><?php
+    ?> <p class = heading> All restaurants of <?php echo $_GET['cuisine'];?> cuisine)</p><?php
       foreach($_GET['cuisine'] as $check) {
     
     
